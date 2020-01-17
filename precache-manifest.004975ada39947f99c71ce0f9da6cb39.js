0e5e4df484513d1a326a688a4e53d88ee638b336
dong-alex <adong@ualberta.ca>
2020-01-16 22:50:02 -0700

    Updates

---
+++ precache-manifest.004975ada39947f99c71ce0f9da6cb39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "329a4665af07e11ab73fa499a43e0bef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab5debb58bd788e9a75e",
+    "url": "/static/js/2.94d450d8.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.94d450d8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a7a5718a96f2b304d70d",
+    "url": "/static/js/main.00724063.chunk.js"
+  },
+  {
+    "revision": "1ae1e48fdd9ef2d7794e",
+    "url": "/static/js/runtime-main.968940bd.js"
+  }
+]);
